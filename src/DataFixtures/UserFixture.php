78966810892098525d94df7a0eb46a838c9f9b86
Sadibou <sadibousarr18@gmail.com>
2020-01-10 13:28:10 +0000

    Fixrure

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Role;
+use App\Entity\User;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+   
+    private $encoder;
+    
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+    public function load(ObjectManager $manager)
+    {
+        $role = new Role();
+        $role->setLibellé("ADMIN_SYS");
+        $manager->persist($role);
+
+        $user = new User();
+        $user->setUsername("supadmin");
+        $user->setPassword($this->encoder->encodePassword($user, "system"));
+        $user->setRoles(array("ROLE_".$role->getLibellé()));
+        $user->setIsActive(true);
+        $user->setProfil($role);
+
+        $manager->persist($user);
+        $manager->flush();
+   
+        $manager->flush();
+
+    }
+}
